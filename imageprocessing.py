b73cb7ec147bb8b5c42cb3ac87b08b61091a09d2
SapphireQuest <turbiarz80@gmail.com>
2026-05-11 13:14:43 +0200

    Feat: Add histogram_equalization function

--- imageprocessing.py
+++ imageprocessing.py
@@ -5,14 +5,14 @@ BLACK = [0,0,0]
 WHITE =[255,255,255]
 GRAY = [128,128,128]
 
-def single_treshold(pixel_array, width, height, treshold1, treshold2):
+def treshold(pixel_array, width, height, treshold1, treshold2):
     for w in range(width):
         for h in range(height):
             pixel = pixel_array[h][w] 
             r = int(pixel[0])
             g = int(pixel[1])
             b = int(pixel[2])
-            avg = (r + g + b)/3
+            avg = (r + g + b)//3
             if treshold2 == -1:
                 if(avg >= treshold1):
                     pixel_array[h][w] = WHITE
@@ -28,15 +28,48 @@ def single_treshold(pixel_array, width, height, treshold1, treshold2):
     return pixel_array
 
 
+def histogram_equalization(pixel_array,width, height):
+    histogram = [0] * 256
+    for w in range(width):
+        for h in range(height):
+            pixel = pixel_array[h][w] 
+            histogram[pixel] += 1
 
+    CDF = [0] * 256
+    CDF[0] = histogram[0]
+    for i in range(1, 256):
+        CDF[i] = CDF[i-1] + histogram[i]
+    for i in range (0, 256):
+        if CDF[i] != 0:
+            CDF_min = CDF[i]
+            break
+    new_shades = [0] * 256
+    for i in range (0, 256):
+        new_value = ((CDF[i] - CDF_min) / (CDF[255] - CDF_min)) * 255
+        new_shades[i] = int(round(new_value))
+    for w in range(width):
+        for h in range(height):
+            pixel = pixel_array[h][w]
+            new_color = new_shades[pixel]
+            pixel_array[h][w] = new_color
+    return pixel_array
 
 def main():
     img = Image.open('yoda.jpeg').convert('RGB')
     width, height = img.size
     pixel_array = np.array(img)
-    pixel_array_copy = pixel_array.copy()
-    single_treshold_array = single_treshold(pixel_array_copy, width, height, 128, 192)
-    single_treshold_image = Image.fromarray(single_treshold_array.astype(np.uint8), 'RGB')
-    single_treshold_image.show()
+    #pixel_array_treshold = pixel_array.copy()
+    #treshold_array = treshold(pixel_array_treshold, width, height, 128, -1)
+    #treshold_image = Image.fromarray(treshold_array.astype(np.uint8), 'RGB')
+    #treshold_image.show()
+
+    grayscale_image = Image.open('yoda.jpeg').convert('L')
+    equalized_pixel_array = np.array(grayscale_image)
+    equalized_array = histogram_equalization(equalized_pixel_array,width, height)
+    grayscale_image.show()
+    equalized_image = Image.fromarray(equalized_array.astype(np.uint8), 'L')
+    equalized_image.show()
+
+
 if __name__ == "__main__":
     main()
